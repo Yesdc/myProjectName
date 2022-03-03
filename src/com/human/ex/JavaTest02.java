08518126b4fb3d28b7b260a08866c409591ae03b
임창우 <@192.168.219.101>
2022-03-03 14:26:16 +0900

    first commit

---
+++ src/com/human/ex/JavaTest02.java
@@ -0,0 +1,86 @@
+package com.human.ex;
+
+public class JavaTest02 {
+
+	public static void main(String[] args) {
+	
+//		1번문제
+//		public class helloWorld {
+//			public static void main(string[] args) {
+//			System.out.println(
+//			"Hello World~"
+//			);
+//			}
+//			}
+		
+
+		
+//		3번문제
+		System.out.println("\"안녕자바\"");
+		System.out.println("");
+		
+//		4번문제
+		System.out.println("임창우");
+		System.out.println("abcdd@naver.com");
+		System.out.println("010-1111-1111");
+		System.out.println("");
+		
+//		5번문제
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("");
+		
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		System.out.println("");
+		
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+		System.out.println("");
+		
+		
+//		6번문제
+		
+		System.out.println("******************");
+		System.out.println("*      성적표      *");
+		System.out.println("******************");
+		System.out.println("* 국어   50       *");
+		System.out.println("******************");
+		System.out.println("* 영어   50       *");
+		System.out.println("******************");
+		System.out.println("* 수학   50       *");
+		System.out.println("******************");
+		System.out.println("* 컴퓨터학과    홍길동*");
+		System.out.println("******************");
+		System.out.println("");
+
+		
+//		7번문제
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/human/ex/javaTest01.java
@@ -0,0 +1,54 @@
+package com.human.ex;
+
+public class javaTest01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+       
+		System.out.println(50+60);
+       
+       if(4>6) {
+    	        	System.out.println(true);
+       }else   {
+    	        	System.out.println(false);    	        
+        }
+       
+       System.out.println("  *");
+       System.out.println(" ***");
+       System.out.println("  *");
+       
+       int a=3;
+       switch(a){
+       	case 1: System.out.println("one");
+       	break;
+       	case 2: System.out.println("two");
+       	break;
+       	case 3: System.out.println("three");
+       
+       }
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+	}
+	
+
+}
