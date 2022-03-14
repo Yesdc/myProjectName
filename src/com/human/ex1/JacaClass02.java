d8c299cfd728058b5c6bde17a8d7b83d718aa587
임창우 <@192.168.219.101>
2022-03-14 15:51:57 +0900

    go

---
+++ src/com/human/ex1/JacaClass02.java
@@ -0,0 +1,179 @@
+package com.human.ex1;
+
+public class JacaClass02 {
+
+	public static void main(String[] args) {
+		
+//		문제1 숫자를 하나 입력 받아 홀수 인지 짝수 인지 구하는 프로그램을 구하여라.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		int num1=Integer.parseInt(sc.nextLine());
+//		System.out.println("숫자입력");
+//		if(num1%2==1) {
+//			System.out.println("홀수");
+//		}else {
+//			System.out.println("짝수");
+//		}
+//		
+		
+		
+//		문제2 숫자 2개를 입력 받아 첫번째 숫자가 두번째 숫자의 배수인지 아닌지 출력하는
+//		프로그램을 구하여라.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		int num1=Integer.parseInt(sc.nextLine());
+//		System.out.println("첫번쨰 숫자입력");
+//		int num2=Integer.parseInt(sc.nextLine());
+//		System.out.println("두번쨰 숫자입력");
+//		if(num1%num2==0) {
+//			System.out.println("첫번째 숫자가 두번째 숫자의 배수");
+//		}else {
+//			System.out.println("배수아님");
+//		}
+		
+		
+		
+//		문제3  a 원을 동전으로 바꾸면 500원짜리 몇 개 100원짜리 몇 개가 되는가?
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("금액 입력");
+//		int a=Integer.parseInt(sc.nextLine());
+//		int m500=a/500;
+//		int m100=(a%500)/100;		
+//		System.out.println("500원"+m500+"개"+" 100원"+m100+"개");
+//				
+		
+		
+//		문제4  500원짜리 n개 100원짜리 m개 총 얼마인가?
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("500원 갯수");
+//		int m500=Integer.parseInt(sc.nextLine());
+//		System.out.println("100원 갯수");
+//		int m100=Integer.parseInt(sc.nextLine());
+//		int result=m500*500+m100*100;
+//		System.out.println(result+"원");
+//		
+		
+		
+//		문제5 n초를 입력 받아 시분초로 바꿔보자.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("초 입력");
+//		int sec=Integer.parseInt(sc.nextLine());
+//		int hour=sec/3600;
+//		int min=(sec%3600)/60;
+//		int sec1=(sec%3600)%60;
+//		System.out.println(hour+"시간 "+min+"분 "+sec1+"초");
+//		
+		
+		
+//		문제6 n시간 m분 l초는 총 몇초인가?
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("시간 입력");
+//		int hour=Integer.parseInt(sc.nextLine());
+//		System.out.println("분 입력");
+//		int min=Integer.parseInt(sc.nextLine());
+//		System.out.println(hour*3600+min*60+1+"초");
+//		
+		
+//		ab 바꾸기
+//		int a=5;
+//		int b=7;
+//		int c=0;
+//		c=a;
+//		a=b;
+//		b=c;
+//		System.out.print(a+"아이고"+b);
+//		
+		
+		
+//		문제1 a, b 의 수를 입력 받아 두 수를 교환한 다음에 두 수를 출력하는 코드를 작성해
+//		보자.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("a 입력");
+//		int a=Integer.parseInt(sc.nextLine());
+//		System.out.println("b 입력");
+//		int b=Integer.parseInt(sc.nextLine());
+//		int c=0;
+//		c=a;
+//		a=b;
+//		b=c;
+//		System.out.println("a="+a+" b="+b);
+			
+		
+//		문제2 a, b중 큰수를 a에 넣어 출력하라
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("a 입력");
+//		int a=Integer.parseInt(sc.nextLine());
+//		System.out.println("b 입력");
+//		int b=Integer.parseInt(sc.nextLine());
+//		int c=0;
+//		if(b>a) {
+//			c=a;
+//			a=b;
+//			b=c;
+//			System.out.println(a);
+//		}else{
+//			System.out.println(a);
+//		}
+
+
+//		문제3 3개의 수를 입력 받아 가장 작은 수를 a, 다음 작은수를 b, 나머지 수를 c에 넣어
+//		작은수부터 출력해 보자. 다음을 만족 시키자.
+//		
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("a 입력");
+//		int a=Integer.parseInt(sc.nextLine());
+//		System.out.println("b 입력");
+//		int b=Integer.parseInt(sc.nextLine());
+//		System.out.println("c 입력");
+//		int c=Integer.parseInt(sc.nextLine());
+//		int d=0;
+//		if(a>b) {
+//			if(a>c) {
+//				if(b>c) {
+//					d=a;
+//					a=c;
+//					c=d;					
+//				}
+//				else {
+//					
+//					d=a;
+//					a=b;
+//					b=d;
+//					d=c;
+//					c=b;
+//					b=d;										
+//				}
+//					
+//			}else { 
+//				d=a;
+//				a=b;
+//				b=d;				
+//			}							
+//		}else if(b>c){
+//				if(a>c) {					
+//					d=a;
+//					a=c;
+//					c=d;
+//					d=c;
+//					c=b;
+//					b=d;					
+//				}else {
+//					d=c;
+//					c=b;
+//					b=d;				
+//				}
+//		}else {			      
+//		}
+//		dd
+//		System.out.print(a+""+b+""+c);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/human/ex1/JavaClass03.java
@@ -0,0 +1,201 @@
+package com.human.ex1;
+
+public class JavaClass03 {
+
+	public static void main(String[] args) {
+
+//			int i=1;//1,0,-1,기타
+//			if(i==1) {
+//				System.out.println("양수");
+//			}else {
+//				if(i==-1) {
+//					System.out.println("음수출력");
+//				}else {
+//					if(i==0) {
+//						System.out.println("0출력");
+//					}else {
+//						System.out.println("약속되지 않은 숫자");
+//					}
+//				}
+//				
+//			}
+//			
+//	이런경우를 대비해 else if문과 switch문이 나옴
+//	switch double,범위조회 사용불가
+			
+			
+//			문제1
+//		int input=0;
+//		int a=5;
+//		int b=7;
+//		if(input==0) {
+//			a=a+2;
+//		}else if(input==1) {
+//			b=b+4;
+//			
+//		}else if(input==2) {
+//			a=a+b;
+//		}else {
+//			b=b+5;
+//		}
+//		System.out.println(a+":"+b);	
+			
+		
+//			문제2 점수를 입력받아 90점이상은 수, 80점이상은 우, 70점 이상은 미 ..로 출력하는
+//			프로그램을 만들어 보자.
+		
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("점수 입력");
+//		int point=Integer.parseInt(sc.nextLine());
+//		int result=point/10;
+//		switch(result) {
+//		case 1:
+//		case 2:
+//		case 3:
+//		case 4:
+//		case 5:
+//		case 6:
+//		case 7:
+//			   System.out.println("미");
+//		break;
+//		case 8:
+//			   System.out.println("우");
+//		break;
+//		case 9:
+//		case 10:
+//			   System.out.println("수");
+//		break;	   
+//			   		   
+//		}
+			
+			
+//			문제3 1~5사이의 숫자를 입력받아 해당 숫자를 한글로 출력하는 프로그램을 작성해 보자.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("1~5 입력");
+//		int num1=Integer.parseInt(sc.nextLine());
+//		switch(num1) {
+//		case 1:
+//			System.out.println("하나");
+//			break;
+//		case 2:
+//			System.out.println("둘");
+//			break;
+//		case 3:
+//			System.out.println("셋");
+//			break;
+//		case 4:
+//			System.out.println("넷");
+//			break;
+//		case 5:
+//			System.out.println("다섯");
+//			break;	
+//		}
+			
+			
+//			문제4 달을 입력받아 해당달이 28일인지 30일인지 31일인지 출력하는 프로그램을 구현해
+//		         보자.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("달 입력");
+//		int month=Integer.parseInt(sc.nextLine());
+//		switch(month) {
+//			case 1:
+//				System.out.println("31일");
+//			break;
+//			case 2:
+//				System.out.println("28일");
+//			break;
+//			case 3:
+//				System.out.println("31일");
+//			break;
+//			case 4:
+//				System.out.println("30일");
+//			break;
+//			case 5:
+//				System.out.println("31일");
+//			break;
+//			case 6:
+//				System.out.println("30일");
+//			break;
+//			case 7:
+//				System.out.println("31일");
+//			break;
+//			case 8:
+//				System.out.println("31일");
+//			break;
+//			case 9:
+//				System.out.println("30일");
+//			break;
+//			case 10:
+//				System.out.println("31일");
+//			break;
+//			case 11:
+//				System.out.println("30일");
+//			break;
+//			case 12:
+//				System.out.println("31일");
+//			break;
+//		}
+			
+			
+//			 문제5 나이를 입력 받아 해당 나이의 때의 학력이 초,중,고 중 어디에 해당하는지
+//		     출력해보자.
+//		java.util.Scanner sc=new java.util.Scanner(System.in);
+//		System.out.println("나이 입력");
+//		int age=Integer.parseInt(sc.nextLine());
+//	    	switch(age) {
+//	    	case 1:
+//	    	case 2:
+//	    	case 3:
+//	    	case 4:
+//	    	case 5:
+//	    	case 6:
+//	    	case 7:
+//	    		System.out.println("아동");
+//	    		break;
+//	    	case 8:
+//	    	case 9:
+//	    	case 10:
+//	    	case 11:
+//	    	case 12:
+//	    	case 13:
+//	    		System.out.println("학력:초");
+//	    		break;
+//	    	case 14:
+//	    	case 15:
+//	    	case 16:
+//	    		System.out.println("학력:중");
+//	    		break;
+//	    	case 17:
+//	    	case 18:
+//	    	case 19:
+//	    		System.out.println("학력:고");
+//	    		break;
+//	    	default:
+//	    		System.out.println("성인");
+//    		break;
+//	    	}
+//			ff
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
