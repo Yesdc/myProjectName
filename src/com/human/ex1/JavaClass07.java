be571806b2107c315a71de055bdee06b581403f3
임창우 <@192.168.219.101>
2022-03-18 14:47:38 +0900

    go

---
+++ src/com/human/ex1/JavaClass07.java
@@ -0,0 +1,111 @@
+package com.human.ex1;
+
+import java.util.Arrays;
+
+public class JavaClass07 {
+
+	public static void main(String[] args) {
+
+//		배열안에 6의배수 10개를 넣은다음 인덱스순으로 출력해보자.
+//		int arr[]=new int[10];		
+//		for(int i=0;i<10;i++) {
+//			arr[i]=6*i;			
+//		}
+//		System.out.println(Arrays.toString(arr));
+//		
+		
+		
+//		문제1 ) 배열 a에 1,2,3 을 넣은 후 배열 내의 모든 값에 2를 더한 값인 3,4,5로 변경한
+//		다음에 배열의 내용을 화면에 출력하는 순서도와 코드를 구현하여 보자.
+		
+//		int arr[]= {1,2,3};
+//		for(int i=0;i<3;i++) {
+//			arr[i]=arr[i]+2;
+//		}
+//		System.out.println(Arrays.toString(arr));
+		
+		
+//		문제2  배열 a[10]에 3의 배수를 넣은 다음에 배열의 내용을 출력하는 순서도와
+//		의사코드를 만들어 보자.
+		
+//		int arr[]=new int[10];
+//		for(int i=0;i<10;i++) {
+//			arr[i]=i*3;
+			
+//		}
+//		System.out.println(Arrays.toString(arr));
+
+		
+//		문제3 배열 a[100]에 1부터 100까지의 숫자를 순서대로 넣은 다음 배열의 인덱스가
+//		짝수인 배열에 들어 있는 값만 출력하는 코드를 만들어 보자.
+		
+//		int arr[]=new int[100];
+//		for(int i=0;i<100;i++) {
+//			arr[i]=i+1;					
+//			if(arr[i]%2==0) {				
+//				System.out.println(arr[i]);				
+//			}						
+//		}
+		
+		
+//		문제4 배열 a[]={12,1,5,3,6,8,5,3}를 만든 다음에 배열의 모든 내용을 더한 값을
+//		sum에 저장하여 출력하는 코드를 만들어 보자.
+		
+//		int a[]= {12,1,5,3,6,8,5,3};
+//		int sum=0;
+//		for(int i=0;i<8;i++) {
+//			sum=sum+a[i];
+//		}
+//		System.out.println(sum);
+		
+		
+//		문제5  a[]={12,1,5,3,6,8,5,3}의 a배열에서 배열 안의 숫자가 짝수인 12,6,8 의 값을
+//		더한 결과 값을 출력하는 프로그램을 작성해 보자.
+		
+//		int a[]={12,1,5,3,6,8,5,3};
+//		int sum=0;
+//		for(int i=0;i<8;i++) {
+//			if(a[i]%2==0) {
+//				sum=sum+a[i];
+//				
+//			}
+//		}
+//		System.out.print(sum);
+		
+		
+//		문제6  a[]={12,1,51,3,6,8,5}의 a배열에서 가장 큰 값과 가장 작은 값을 더하는
+//		프로그램을 작성해 보자.
+//		int a[]={12,1,51,3,6,8,5};
+//		int num=a[0];
+//		int num2=a[0];
+//		for(int i=0;i<7;i++) {
+//			if(num>a[i]) {
+//				num=a[i];
+//			}
+//			if(num2<a[i]) {
+//				num2=a[i];
+//			}
+//		}
+//		System.out.println(num+num2); 
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
