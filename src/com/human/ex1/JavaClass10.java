bd1100ea151096446c72b3a455d38a2257e01db3
admin <admin@DESKTOP-LTC1204>
2022-03-23 13:56:22 +0900

    hh

---
+++ src/com/human/ex1/JavaClass10.java
@@ -0,0 +1,360 @@
+package com.human.ex1;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class JavaClass10 {
+
+			
+	public static void main(String[] args) {
+		
+		
+//		for(int i=0;i<7;i++) {
+//			for(int j=0;j<i+1;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+		
+//		for(int i=0;i<7;i++) {
+//			for(int j=0;j<7-i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+		
+//		for(int i=0;i<7;i++) {
+//			for(int j=i+1;j<7;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//				
+//			}
+//			System.out.println("");
+//		}
+		
+	
+//		q1
+//		for(int i=0;i<7;i++) {
+//			System.out.print("*");
+//		}
+		
+//		q2
+//		for(int i=0;i<7;i++) {
+//			System.out.println("*");
+//		}
+		
+//		q3
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<7;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q4
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<7;a++) {
+//				System.out.print(i+1);
+//			}
+//			System.out.println("");
+//		}
+		
+//		q5
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<7;a++) {
+//				System.out.print(a);
+//			}
+//			System.out.println("");
+//		}
+		
+//		q6
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<7;a++) {
+//				System.out.print(a+i);
+//			}
+//			System.out.println("");
+//		}
+		
+//		q7
+//		for(int i=0;i<5;i++) {
+//			for(int a=5;a<10;a++) {
+//				System.out.print(a-i);
+//			}
+//			System.out.println("");
+//		}
+		
+//		q8
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q9
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<7-i;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q10
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<i;a++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<7-i;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q11
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<6-i;a++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q12
+//		for(int i=0;i<7;i++) {			
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}			
+//			System.out.println("");
+//		}				
+//		for(int x=0;x<6;x++) {
+//			for(int c=0;c<6-x;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q13
+//		for(int i=0;i<7;i++) {			
+//			for(int a=i;a<7;a++) {
+//				System.out.print("*");
+//			}			
+//			System.out.println("");
+//		}				
+//		for(int x=0;x<6;x++) {
+//			for(int c=0;c<x+2;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q14
+//		for(int i=0;i<7;i++) {
+//			for(int a=0;a<i;a++) {
+//				System.out.print(" ");
+//			}			
+//			for(int a=i;a<7;a++) {
+//				System.out.print("*");
+//			}			
+//			System.out.println("");
+//		}				
+//		for(int x=0;x<6;x++) {
+//			for(int a=x;a<5;a++) {
+//				System.out.print(" ");
+//			}			
+//			for(int c=0;c<x+2;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q15
+//		for(int i=0;i<5;i++) {
+//			for(int b=i+1;b<5;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int i=0;i<5;i++) {
+//			for(int b=0;b<i+1;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=i;a<4;a++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q16
+//		for(int i=0;i<5;i++) {
+//			for(int b=i;b<4;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}
+//			for(int c=0;c<i;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q17
+//		for(int i=0;i<5;i++) {
+//			for(int a=0;a<i;a++) {
+//				System.out.print(" ");
+//			}
+//			for(int b=i;b<5;b++) {
+//				System.out.print("*");
+//			}
+//			for(int c=i;c<4;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q18
+//		for(int i=0;i<5;i++) {
+//			for(int b=i;b<4;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print("*");
+//			}
+//			for(int c=0;c<i;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int i=0;i<5;i++) {
+//			for(int a=0;a<i+1;a++) {
+//				System.out.print(" ");
+//			}
+//			for(int b=i;b<4;b++) {
+//				System.out.print("*");
+//			}
+//			for(int c=i;c<3;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q19	
+		
+//		for(int i=0;i<5;i++) {
+//			for(int a=i;a<5;a++) {
+//				System.out.print("*");
+//			}
+//			for(int b=0;b<i+i+1;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int c=i;c<5;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int d=0;d<4;d++) {
+//			for(int e=0;e<d+2;e++) {
+//				System.out.print("*");
+//			}
+//			for(int f=d+d;f<7;f++) {
+//				System.out.print(" ");
+//			}
+//			for(int g=0;g<d+2;g++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q20
+//		for(int d=0;d<5;d++) {
+//			for(int e=0;e<d+1;e++) {
+//				System.out.print("*");
+//			}
+//			for(int f=d+d;f<9;f++) {
+//				System.out.print(" ");
+//			}
+//			for(int g=0;g<d+1;g++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int i=0;i<4;i++) {
+//			for(int a=i;a<4;a++) {
+//				System.out.print("*");
+//			}
+//			for(int b=0;b<i+i+3;b++) {
+//				System.out.print(" ");
+//			}
+//			for(int c=i;c<4;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+//		q21
+//		for(int i=0;i<5;i++) {
+//			for(int a=0;a<i;a++) {
+//				System.out.print(" ");
+//			}
+//			for(int b=i;b<5;b++) {
+//				System.out.print("*");
+//			}
+//			for(int c=i;c<4;c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int d=0;d<4;d++) {
+//			for(int e=d;e<3;e++) {
+//				System.out.print(" ");
+//			}
+//			for(int f=0;f<d+2;f++) {
+//				System.out.print("*");
+//			}
+//			for(int g=0;g<d+1;g++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+		
+//		int ar[]={3,6,4,2,7,8,1,5,};
+//		for(int i=0;i<ar.length-1;i++) {
+//			for(int a=0;a<ar.length-1;a++) {
+//				if(ar[a]>ar[a+1]) {
+//					int temp=ar[a];
+//					ar[a]=ar[a+1];
+//					ar[a+1]=temp;
+//				}	
+//				
+//			}
+//	
+//		}
+//		System.out.print(Arrays.toString(ar));
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/human/ex1/JavaClass11.java
@@ -0,0 +1,33 @@
+package com.human.ex1;
+
+import java.util.Arrays;
+
+public class JavaClass11 {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {6,5,1,8,7,4,2,3};
+		
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=0;j<arr.length-1;i++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}dd
+			}
+		}
+		System.out.print(Arrays.toString(arr));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
