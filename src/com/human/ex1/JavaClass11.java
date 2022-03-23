bf9f42f60a4001044fcdf3285061a87f4e2964ab
admin <admin@DESKTOP-LTC1204>
2022-03-23 15:29:39 +0900

    gogo

--- src/com/human/ex1/JavaClass11.java
+++ src/com/human/ex1/JavaClass11.java
@@ -6,18 +6,56 @@ public class JavaClass11 {
 
 	public static void main(String[] args) {
 		
-		int arr[]= {6,5,1,8,7,4,2,3};
+//		int arr[]= {6,5,1,8,7,4,2,3};
+//		System.out.println(Arrays.toString(arr));
+//		for(int i=0;i<arr.length-1;i++) {
+//			for(int j=0;j<arr.length-1;j++) {
+//				if(arr[j]>arr[j+1]) {
+//					int temp=arr[j];
+//					arr[j]=arr[j+1];
+//					arr[j+1]=temp;
+//					System.out.println(Arrays.toString(arr));
+//				}
+//			}
+//		}
+//		System.out.print(Arrays.toString(arr));
+//		
+//		
 		
-		for(int i=0;i<arr.length-1;i++) {
-			for(int j=0;j<arr.length-1;i++) {
-				if(arr[j]>arr[j+1]) {
-					int temp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
-				}dd
+		int arr[]= {6,5,1,8,7,4,2,3};
+	
+		System.out.println(Arrays.toString(arr));
+		for(int i=7;i>0;i--) {
+			int maxIndex=0;		
+			int selectIndex=i;
+			for(int j=0;j<=selectIndex;j++) {
+				if(arr[j] > arr[maxIndex]) {
+					
+					maxIndex=j;
+				}
 			}
+			
+			int temp=arr[maxIndex];
+			arr[maxIndex]=arr[selectIndex];
+			arr[selectIndex]=temp;
+			System.out.println(Arrays.toString(arr));
 		}
-		System.out.print(Arrays.toString(arr));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
